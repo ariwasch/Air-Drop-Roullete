d5c0c771bdf7301a26f005de439e3d0f1b3401b2
Ari Wasch <ariwasch@gmail.com>
2020-03-06 21:40:12 -0500

    Initial Commit

---
+++ AirDrop2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirDrop2
+//
+//  Created by Ari Wasch on 3/6/20.
+//  Copyright © 2020 Ari Wasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
